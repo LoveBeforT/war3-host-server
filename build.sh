841cae2f96a57613bca7f1cb5883388d46bf5445
KouKouChan <1010433454@qq.com>
2022-08-27 12:18:12 +0800

    add: build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+cd `dirname $0` && pwd
+
+rm -rf output/
+mkdir output/
+
+if [ ! -n "$1" ] ;then
+    cargo build
+    mv target/debug/war3-host-server output/server
+else
+    cargo build --$1
+    mv target/$1/war3-host-server output/server
+fi
+
+cp config/* output/
+
+cargo clean
